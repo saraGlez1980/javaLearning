d0dab038e2d0b6142661e5f4bcbdb315c2fa321a
sari <sari@DESKTOP-U7TGTT1.mshome.net>
2025-02-24 18:09:29 +0100

    añadiendo ejemplos capitulo03
    añadiendo ejemplos del capitulo04
    
    añadiendo ejemplo cap05. piedra papel tijera

---
+++ javaLearning/src/javaLearning/cap03/Ejemplo03_01.java
@@ -0,0 +1,13 @@
+package javaLearning.cap03;
+
+public class Ejemplo03_01 {
+    public static void main(String[] args) {
+        int temp = Integer.valueOf(args[0]);
+        System.out.print("La temperatura es de ");
+        if (temp > 0) {
+            System.out.println(temp + "ºC positivos.");
+        } else {
+            System.out.println(temp + "ºC bajo cero.");
+        }
+    }
+}
