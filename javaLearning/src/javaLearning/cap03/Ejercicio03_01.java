e2c20194d598f17cdd58c49b8c6d82a407d8ae8e
saraGlez1980 <sara.glez.cuervo@gmail.com>
2025-02-25 12:36:10 +0100

    modificado primer ejercicio capitulo 03

--- javaLearning/src/javaLearning/cap03/Ejercicio03_01.java
+++ javaLearning/src/javaLearning/cap03/Ejercicio03_01.java
@@ -5,14 +5,6 @@
 
 public class Ejercicio03_01 {
 	public static void main(String[] args) {
-		int numArgs = args.length;
-		// en la rama if, pondremos una condición
-		if (numArgs == 0) {
-			// System.err.println sacará el mensaje por la salida de error
-			// En este caso, por la consola, pero en rojo
-			System.err.println("No se han recibido argumentos");
-		} else { // la rama else se ejecutará en el resto de casos
-			System.out.println("Se han recibido " + numArgs + " argumentos");
-		}
+		
 	}
 }
